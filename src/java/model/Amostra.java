79542528f932a8998fb724ab815d1715091ac3d7
Matheus <mathewskta@gmail.com>
2018-04-29 22:20:37 -0300

    DIFF_AMOSTRA_ANALISE

--- src/java/model/Amostra.java
+++ src/java/model/Amostra.java
@@ -7,22 +7,9 @@ public class Amostra {
     private int codigo;
     private int codigoPedido;
     private int codigoProfissionalColeta;
-    private int codigoProfissionalAnalise;
-    private Double iqa;
-    private Double fosfatos;
-    private Double turbidez;
-
+    
     public Amostra() {
     }
-
-    public Amostra(int codigoPedido, int codigoProfissionalColeta, Double iqa, Double fosfatos, Double turbidez) {
-        this.codigoPedido = codigoPedido;
-        this.codigoProfissionalColeta = codigoProfissionalColeta;
-        this.iqa = iqa;
-        this.fosfatos = fosfatos;
-        this.turbidez = turbidez;
-    }
-
     public int getCodigo() {
         return codigo;
     }
@@ -46,36 +33,5 @@ public class Amostra {
     public void setCodigoProfissionalColeta(int codigoProfissionalColeta) {
         this.codigoProfissionalColeta = codigoProfissionalColeta;
     }
-
-    public int getCodigoProfissionalAnalise() {
-        return codigoProfissionalAnalise;
-    }
-
-    public void setCodigoProfissionalAnalise(int codigoProfissionalAnalise) {
-        this.codigoProfissionalAnalise = codigoProfissionalAnalise;
-    }
-
-    public Double getIqa() {
-        return iqa;
-    }
-
-    public void setIqa(Double iqa) {
-        this.iqa = iqa;
-    }
-
-    public Double getFosfatos() {
-        return fosfatos;
-    }
-
-    public void setFosfatos(Double fosfatos) {
-        this.fosfatos = fosfatos;
-    }
-
-    public Double getTurbidez() {
-        return turbidez;
-    }
-
-    public void setTurbidez(Double turbidez) {
-        this.turbidez = turbidez;
-    }   
+    
 }
